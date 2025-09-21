ef96c51b360d50027ceb09856e35940aea46dc20
Viktor Momotenko <viktor.momotenko@gmail.com>
2025-09-22 00:19:44 +0100

    feat(feeling): implement feeling check card component

--- components/FeelingCheckCard/FeelingCheckCard.tsx
+++ components/FeelingCheckCard/FeelingCheckCard.tsx
@@ -1,7 +1,33 @@
-const FeelingCheckCard = () => {
-    return (
-        <div>FeelingCheckCard</div>
-    )
-}
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useAuthStore } from '@/app/store/useAuthStore';
+import styles from './FeelingCheckCard.module.css';
+import React, { FC } from 'react';
+
+const FeelingCheckCard: FC = () => {
+  const { isLoggedIn } = useAuthStore();
+  const router = useRouter();
+
+  const handleButtonClick = () => {
+    if (isLoggedIn) {
+      console.log('Open feeling check modal for authorized user');
+    } else {
+      router.push('/auth/signin');
+    }
+  };
+
+  return (
+    <div className={styles.card}>
+      <h3 className={styles.question}>Як ти себе почуваєш сьогодні?</h3>
+      <p className={styles.recommendation}>
+        Заплануй свою ідеальну рутину, щоб бути продуктивним і щасливим.
+      </p>
+      <button onClick={handleButtonClick} className={styles.button}>
+        Запланувати
+      </button>
+    </div>
+  );
+};
 
 export default FeelingCheckCard;
