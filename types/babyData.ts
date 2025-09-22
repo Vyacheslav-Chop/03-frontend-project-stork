639ebca852fbe43f79533ba2a2abf615831ff102
Dmitry Zinchenko <zinchenko89d@gmail.com>
2025-09-22 11:14:11 +0300

    add type BabyState

---
+++ types/babyData.ts
@@ -0,0 +1,8 @@
+export interface BabyData {
+    weekNumber: number;
+    babySize: number | string;
+    babyWeight: number | string;
+    image: string;
+    babyActivity: string;
+    babyDevelopment: string;
+}
