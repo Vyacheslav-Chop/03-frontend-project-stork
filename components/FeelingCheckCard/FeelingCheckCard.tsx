90cebc77b794864fcdb435e652e492310f9c0290
Viktor Momotenko <viktor.momotenko@gmail.com>
2025-09-23 13:17:21 +0100

    refactor: improve FeelingCheckCard component

--- components/FeelingCheckCard/FeelingCheckCard.tsx
+++ components/FeelingCheckCard/FeelingCheckCard.tsx
@@ -1,30 +1,25 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { useAuthStore } from '@/app/store/useAuthStore';
 import styles from './FeelingCheckCard.module.css';
 import React, { FC } from 'react';
 
-const FeelingCheckCard: FC = () => {
-  const { isLoggedIn } = useAuthStore();
-  const router = useRouter();
+interface FeelingCheckCardProps {
+  onButtonClick: () => void;
+}
 
-  const handleButtonClick = () => {
-    if (isLoggedIn) {
-      console.log('Open feeling check modal for authorized user');
-    } else {
-      router.push('/auth/signin');
-    }
-  };
-
+const FeelingCheckCard: FC<FeelingCheckCardProps> = ({ onButtonClick }) => {
   return (
     <div className={styles.card}>
-      <h3 className={styles.question}>Як ти себе почуваєш сьогодні?</h3>
-      <p className={styles.recommendation}>
-        Заплануй свою ідеальну рутину, щоб бути продуктивним і щасливим.
-      </p>
-      <button onClick={handleButtonClick} className={styles.button}>
-        Запланувати
+      <div className={styles.textContainer}>
+        <h3 className={styles.question}>Як ви себе почуваєте?</h3>
+        <div className={styles.recommendation}>
+          <span className={styles.recommendationHeading}>Рекомендації на сьогодні:</span>
+          <span className={styles.recommendationText}>Занотуйте незвичні відчуття у тілі.</span>
+        </div>
+      </div>
+
+      <button onClick={onButtonClick} className={styles.button}>
+        Зробити запис у щоденник
       </button>
     </div>
   );
