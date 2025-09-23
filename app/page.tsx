0b77f4d37962c9ea314ad98a1a87d1c0930993c7
Viktor Momotenko <viktor.momotenko@gmail.com>
2025-09-23 16:03:45 +0100

    ref: move button logic to component

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,12 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { useAuthStore } from '@/app/store/useAuthStore';
 import FeelingCheckCard from '@/components/FeelingCheckCard/FeelingCheckCard';
-// import AddDiaryEntryModal ...
 
 export default function Home() {
-  const { isLoggedIn } = useAuthStore();
-  const router = useRouter();
-
-  // const [isModalOpen, setIsModalOpen] = useState(false);
-
-  // const handleCloseModal = () => {
-  //   setIsModalOpen(false);
-  // };
-
-  const handleFeelingCheckClick = () => {
-    if (isLoggedIn) {
-      console.log('Open feeling check modal for authorized user');
-      //  setIsModalOpen(true);
-    } else {
-      router.push('/auth/sign-up');
-    }
-  };
-
   return (
     <div>
       <div>Home</div>
-      <FeelingCheckCard onButtonClick={handleFeelingCheckClick} />
-
-      {/* {isModalOpen && <AddDiaryEntryModal onClose={handleCloseModal} />} */}
+      <FeelingCheckCard />
     </div>
   );
 }
